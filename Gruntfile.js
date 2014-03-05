6627e8e24ce7717183a733f32dfe907e4c6a24dd
Kuba Jagoda <kuba.jagoda@gmail.com>
2014-03-05 21:07:54 +0100

    Initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        clean: ['src/*.js', 'src/*.map', 'test/spec/*.js', 'test/spec/*.map'],
+        karma: {
+            continuous: {
+                configFile: 'test/karma.conf.js',
+                autoWatch: true,
+                singleRun: false
+            },
+            unit: {
+                configFile: 'test/karma.conf.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-karma');
+
+    grunt.registerTask('test', function (type) {
+        if (type === 'continuous') {
+            grunt.task.run('karma:continuous');
+        } else {
+            grunt.task.run('karma:unit');
+        }
+    });
+};
